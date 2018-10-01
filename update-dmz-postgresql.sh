5dfc4b3e22c6a7500a434c3e68679558645cbfb5
Gia Thanh Vuong <tvuong@intelliviewtech.com>
2018-10-01 15:46:36 -0600

    fix missing slash wget

--- update-dmz-postgresql.sh
+++ update-dmz-postgresql.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # -N is to update, drop -N to download all > 15 GB 
 wget --no-parent -r -N http://apt.postgresql.org/pub/repos/apt/pool/ -P mirror/
-wget --no-parent -r -N http://apt.postgresql.org/pub/repos/apt/dists/xenial-pgdg -P mirror/
+wget --no-parent -r -N http://apt.postgresql.org/pub/repos/apt/dists/xenial-pgdg/ -P mirror/
 
 # try this -m as sync 
 # wget --no-parent -m -N http://apt.postgresql.org/pub/repos/apt/pool/ -P mirror/
